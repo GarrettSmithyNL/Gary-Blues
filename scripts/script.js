0b9c3f676f6a7449e80b8ee79d985fff0ebc6494
garrettsmithynl <garrett_smith_30@hotmail.com>
2024-02-21 17:05:09 -0330

    added JSON file for price list, added ordering menu with functionality to update quantitiy.

---
+++ scripts/script.js
@@ -0,0 +1,89 @@
+let plusElements = document.querySelectorAll(".plus");
+let minusElements = document.querySelectorAll(".minus");
+
+let summary = document.querySelector("#summary");
+
+plusElements.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    //Split the element id into smaller peices
+    const split = element.id.split("-");
+
+    //Gets the quantity box
+    const quantity = document.querySelector(
+      `#quantity-${split[1]}-${split[2]}`
+    );
+
+    //Gets the name of the menu item
+    const name = document.querySelector(
+      `#menu-${split[1]}-${split[2]}`
+    ).innerText;
+
+    //removes whitespace from id name
+    const split2 = name.split(" ");
+    let newID = "";
+    split2.forEach((element) => {
+      newID = newID + element;
+    });
+
+    //update the ordered quantity
+    quantity.innerHTML = parseInt(quantity.innerHTML) + 1;
+
+    //update Summary Screen
+    if (quantity.innerHTML == 1) {
+      console.log("Made it!");
+      //create a new div for the entry in the summary
+      const node = document.createElement("div");
+      //sets the id of the element to name
+      node.setAttribute("id", newID);
+      //Create the text for the summart
+      const text = document.createTextNode(
+        `${name}   ${parseInt(quantity.innerHTML)} @ 9.99`
+      );
+      //Adds the text to the new div and add the new div to the summary
+      node.appendChild(text);
+      summary.appendChild(node);
+    } else if (quantity.innerHTML > 1) {
+      console.log("Also Made it here.");
+      var summaryItem = document.querySelector(`#${newID}`);
+      summaryItem.innerText = `${name}   ${parseInt(
+        quantity.innerHTML
+      )} @ 9.99`;
+    }
+  });
+});
+
+minusElements.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    //Split the element id into smaller peices
+    let split = element.id.split("-");
+
+    //Gets the quantity box
+    let quantity = document.querySelector(`#quantity-${split[1]}-${split[2]}`);
+
+    //Gets the name of the menu item
+    const name = document.querySelector(
+      `#menu-${split[1]}-${split[2]}`
+    ).innerText;
+
+    //removes whitespace from id name
+    const split2 = name.split(" ");
+    let newID = "";
+    split2.forEach((element) => {
+      newID = newID + element;
+    });
+
+    if (parseInt(quantity.innerHTML) >= 1) {
+      //update the ordered quantity
+      quantity.innerHTML = parseInt(quantity.innerHTML) - 1;
+      //updates the summary
+      let summaryItem = document.querySelector(`#${newID}`);
+      summaryItem.innerText = `${name}   ${parseInt(
+        quantity.innerHTML
+      )} @ 9.99`;
+    }
+    //removes the item from the summary if 0 is selected
+    if (parseInt(quantity.innerHTML) < 1) {
+      document.querySelector(`#${newID}`).remove();
+    }
+  });
+});
