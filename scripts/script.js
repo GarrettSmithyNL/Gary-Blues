ce159addd3044727508c18b3823165a6ad636dcb
garrettsmithynl <garrett_smith_30@hotmail.com>
2024-02-22 12:13:39 -0330

    finished the order summary page.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
+let TAX_RATE = 0.15;
+
 let plusElements = document.querySelectorAll(".plus");
 let minusElements = document.querySelectorAll(".minus");
-
 let summary = document.querySelector("#summary");
+
+let formatter = new Intl.NumberFormat("es-US", {
+  minimumFractionDigits: 2,
+  useGrouping: false,
+});
 
 plusElements.forEach((element) => {
   element.addEventListener("click", (event) => {
@@ -18,6 +24,13 @@ plusElements.forEach((element) => {
       `#menu-${split[1]}-${split[2]}`
     ).innerText;
 
+    //Gets the price of the item
+    const dirtyPrice = document.querySelector(
+      `#cost-${split[1]}-${split[2]}`
+    ).innerText;
+    const arrPrice = dirtyPrice.split(" ");
+    const price = arrPrice[1];
+
     //removes whitespace from id name
     const split2 = name.split(" ");
     let newID = "";
@@ -30,24 +43,27 @@ plusElements.forEach((element) => {
 
     //update Summary Screen
     if (quantity.innerHTML == 1) {
-      console.log("Made it!");
       //create a new div for the entry in the summary
       const node = document.createElement("div");
       //sets the id of the element to name
       node.setAttribute("id", newID);
+      node.setAttribute("class", "summaryItem");
+      // node.setAttribute("class", "summaryItem");
       //Create the text for the summart
       const text = document.createTextNode(
-        `${name}   ${parseInt(quantity.innerHTML)} @ 9.99`
+        `${name} ${parseInt(quantity.innerHTML)} @ $${price}`
       );
+
       //Adds the text to the new div and add the new div to the summary
       node.appendChild(text);
       summary.appendChild(node);
+      updateTotals(price, "plus");
     } else if (quantity.innerHTML > 1) {
-      console.log("Also Made it here.");
       var summaryItem = document.querySelector(`#${newID}`);
-      summaryItem.innerText = `${name}   ${parseInt(
+      summaryItem.innerText = `${name} ${parseInt(
         quantity.innerHTML
-      )} @ 9.99`;
+      )} @ $${price}`;
+      updateTotals(price, "plus");
     }
   });
 });
@@ -65,6 +81,13 @@ minusElements.forEach((element) => {
       `#menu-${split[1]}-${split[2]}`
     ).innerText;
 
+    //Gets the price of the item
+    const dirtyPrice = document.querySelector(
+      `#cost-${split[1]}-${split[2]}`
+    ).innerText;
+    const arrPrice = dirtyPrice.split(" ");
+    const price = arrPrice[1];
+
     //removes whitespace from id name
     const split2 = name.split(" ");
     let newID = "";
@@ -79,7 +102,8 @@ minusElements.forEach((element) => {
       let summaryItem = document.querySelector(`#${newID}`);
       summaryItem.innerText = `${name}   ${parseInt(
         quantity.innerHTML
-      )} @ 9.99`;
+      )} @ $${price}`;
+      updateTotals(price, "minus");
     }
     //removes the item from the summary if 0 is selected
     if (parseInt(quantity.innerHTML) < 1) {
@@ -87,3 +111,34 @@ minusElements.forEach((element) => {
     }
   });
 });
+
+let updateTotals = (price, mode) => {
+  let subTotalSection = document.querySelector("#sub-total");
+  let subTotalText = subTotalSection.innerText;
+  const subArr = subTotalText.split(" ");
+  let subTotal = parseFloat(subArr[2]);
+
+  let taxesSection = document.querySelector("#taxes");
+
+  let grandTotalSection = document.querySelector("#grand-total");
+
+  console.log(subTotal);
+  if (mode === "plus") {
+    subTotal = parseFloat(subTotal) + parseFloat(price);
+  } else if (mode === "minus") {
+    subTotal = parseFloat(subTotal) - parseFloat(price);
+  }
+
+  let taxes = parseFloat(subTotal) * TAX_RATE;
+  console.log(taxes);
+  let grandTotal = parseFloat(taxes) + parseFloat(subTotal);
+  console.log(grandTotal);
+
+  let subTemp = Math.round(subTotal * 100) / 100;
+  let taxTemp = Math.round(taxes * 100) / 100;
+  let grandTemp = Math.round(grandTotal * 100) / 100;
+
+  subTotalSection.innerText = `Sub-Total: $ ${formatter.format(subTemp)}`;
+  taxesSection.innerText = `Taxes: $ ${formatter.format(taxTemp)}`;
+  grandTotalSection.innerText = `Total: $ ${formatter.format(grandTemp)}`;
+};
